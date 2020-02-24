1dc6d9e0ca2feaf22e80607a0c4618163a255f96
mttran123 <tranthanhmai88@gmail.com>
2020-02-24 13:20:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gohasu/toastdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.gohasu.toastdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
+        Log.i("Info","Click Me pressed");
+
+        Log.i("Name",nameEditText.getText().toString());
+
+        Toast.makeText(this, "Hello "+nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
